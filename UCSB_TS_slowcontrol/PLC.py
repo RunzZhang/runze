34e16c0bf3a6de373e3b65cb571f85edd1f55757
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2023-02-13 14:18:45 -0800

    Update PLC.py

--- UCSB_TS_slowcontrol/PLC.py
+++ UCSB_TS_slowcontrol/PLC.py
@@ -178,7 +178,7 @@ class PLC:
         #         pass
         # print(result)
 
-        Raw_BO_TT_BO = self.Client_BO.read_holding_registers(50, count=2, unit=0x01)
+        Raw_BO_TT_BO = self.Client_BO.read_holding_registers(94, count=2, unit=0x01)
         print(Raw_BO_TT_BO)
         TT_BO_dic = round(
             struct.unpack(">f", struct.pack(">HH", Raw_BO_TT_BO.getRegister(1), Raw_BO_TT_BO.getRegister(0)))[
