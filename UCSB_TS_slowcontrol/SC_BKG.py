3e173005f495d75ed39a007f7670cb5c1cf5dba3
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2023-09-11 14:08:47 -0700

    retest whether it is LS issue

--- UCSB_TS_slowcontrol/SC_BKG.py
+++ UCSB_TS_slowcontrol/SC_BKG.py
@@ -2557,7 +2557,7 @@ class UpdatePLC(QtCore.QObject):
                     print("PLC updating", datetime.datetime.now())
                     self.PLC.ReadAll()
                     self.PLC.Read_AD()
-                    self.PLC.Read_LS()
+                    # self.PLC.Read_LS()
                     # self.PLC.Read_LL()
                     self.PLC.UpdateSignal()
                     print("finished")
