b93f93802379760edb87d7daba7f09819d38d952
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2023-11-18 10:39:09 -0600

    Update SC_BKG.py

--- UCSB_TS_slowcontrol/SC_BKG.py
+++ UCSB_TS_slowcontrol/SC_BKG.py
@@ -3161,12 +3161,12 @@ class UpdateServer(QtCore.QObject):
             self.TT_AD1_LowLimit_ini[key] = self.PLC.TT_AD1_LowLimit[key]
         for key in self.PLC.TT_AD2_LowLimit:
             self.TT_AD2_LowLimit_ini[key] = self.PLC.TT_AD2_LowLimit[key]
-        for key in self.PLC.LL_LowLimit:
-            self.LL_LowLimit_ini[key] = self.PLC.LL_LowLimit[key]
+        # for key in self.PLC.LL_LowLimit:
+        #     self.LL_LowLimit_ini[key] = self.PLC.LL_LowLimit[key]
         for key in self.PLC.PT_LowLimit:
             self.PT_LowLimit_ini[key] = self.PLC.PT_LowLimit[key]
-        # for key in self.PLC.LEFT_REAL_LowLimit:
-        #     self.LEFT_REAL_LowLimit_ini[key] = self.PLC.LEFT_REAL_LowLimit[key]
+        for key in self.PLC.LEFT_REAL_LowLimit:
+            self.LEFT_REAL_LowLimit_ini[key] = self.PLC.LEFT_REAL_LowLimit[key]
 
 
         for key in self.PLC.TT_AD1_Activated:
