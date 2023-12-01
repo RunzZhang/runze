9fee3f16448388eb24dfe096e11e43ac58cc8d51
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2023-11-30 18:48:59 -0600

    Update SC_BKG.py

--- UCSB_TS_slowcontrol/SC_BKG.py
+++ UCSB_TS_slowcontrol/SC_BKG.py
@@ -561,7 +561,11 @@ class PLC(QtCore.QObject):
         # test part
         self.socket_LS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket_LS.connect((self.IP_LS2, self.PORT_LS1))
-        print("connection success!")
+        print("connection success!1")
+        self.socket_LS.close()
+        self.socket_LS = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket_LS.connect((self.IP_LS2, self.PORT_LS1))
+        print("connection success!2")
         command = "KRDG?0\n"
         cm_code = command.encode()
         self.socket_LS.send(cm_code)
