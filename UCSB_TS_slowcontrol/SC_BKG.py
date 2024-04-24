a914b1ad68fa498157ad74b73ee8669cd0524a6f
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-04-24 15:24:12 -0500

    Update SC_BKG.py

--- UCSB_TS_slowcontrol/SC_BKG.py
+++ UCSB_TS_slowcontrol/SC_BKG.py
@@ -691,6 +691,8 @@ class PLC(QtCore.QObject):
                 print(2)
                 self.socket_LS2.send(cm_code)
                 print(3)
+                data = self.socket_LS2.recv(self.BUFFER_SIZE)
+                print(4,data)
                 output_tuple = LS_TT_translate(self.socket_LS2.recv(self.BUFFER_SIZE).decode())
                 # combining 2nd digit and 3rd digit to get final address
                 print(1)
