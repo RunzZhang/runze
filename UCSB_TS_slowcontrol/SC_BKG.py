55f85f7fe54eb37e558c11194e56a23afe62a2e4
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-04-26 14:23:01 -0500

    Update SC_BKG.py

--- UCSB_TS_slowcontrol/SC_BKG.py
+++ UCSB_TS_slowcontrol/SC_BKG.py
@@ -587,8 +587,8 @@ class PLC(QtCore.QObject):
         command = "KRDG? 0\r\n"
         cm_code = command.encode('utf-8')
         self.socket_LS.send(cm_code)
-        # receive = self.socket_LS.recv(self.BUFFER_SIZE).decode('utf-8')
-        receive = self.socket_LS.recv(self.BUFFER_SIZE).decode()
+        receive = self.socket_LS.recv(self.BUFFER_SIZE).decode('utf-8')
+        # receive = self.socket_LS.recv(self.BUFFER_SIZE).decode()
         print("decode", receive)
         self.socket_LS.close()
         print("connection success!3")
