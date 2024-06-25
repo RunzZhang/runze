fa47991285cfd7146d3d908a4b192c8b840d1f26
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-06-25 12:50:49 -0700

    Update grab_data_from_mysql.py

--- UCSB_TS_slowcontrol/grab_data_from_mysql.py
+++ UCSB_TS_slowcontrol/grab_data_from_mysql.py
@@ -1,10 +1,10 @@
-import DataBase_SBC
+import DataBase_UCSB
 instrument = input("Enter the instrument PID: ")
 start_date = input("Enter the start date (YYYY-MM-DD): ")
 end_date = input("Enter the end date (YYYY-MM-DD): ")
 
 filename='output'+instrument+'.csv'
 
-DB =  DataBase_SBC.mydatabase()
+DB =  DataBase_UCSB.mydatabase()
 DB.select_export_data_datastorage(instrument,start_date,end_date,filename)
 print("Done! files is written to ~/Downloads/"+filename)
