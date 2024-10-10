3af3dc4ce8491d829413f6fd4c5fb51d53b7babd
RunzZhang <52052572+RunzZhang@users.noreply.github.com>
2024-10-10 10:56:52 -0700

    Update SC_BKG.py

--- UCSB_TS_slowcontrol/SC_BKG.py
+++ UCSB_TS_slowcontrol/SC_BKG.py
@@ -962,10 +962,10 @@ class PLC(QtCore.QObject):
             # Reading all the RTDs
 
             for key in self.TT_AD2_address:
-                Raw_RTDs_AD2[key] = self.Client_AD2.read_holding_registers(self.TT_AD2_address[key], count=2, unit=0x01)
+                Raw_RTDs_AD2[key] = self.Client_AD2.read_holding_registers(self.TT_AD2_address[key], count=1, unit=0x01)
                 # also transform C into K if value is not NULL
-                read_value = round(struct.unpack("<f", struct.pack("<HH", Raw_RTDs_AD2[key].getRegister(1), Raw_RTDs_AD2[key].getRegister(0)))[0], 3)
-
+                # read_value = round(struct.unpack("<f", struct.pack("<HH", Raw_RTDs_AD2[key].getRegister(1), Raw_RTDs_AD2[key].getRegister(0)))[0], 3)
+                read_value = -200 + 400 * Raw_RTDs_AD2[key].getRegister(0) / 2 ** 16
                 if read_value < 201:
 
                     self.TT_AD2_dic[key] = round(273.15 + read_value,1)
